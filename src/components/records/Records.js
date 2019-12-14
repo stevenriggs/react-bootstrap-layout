90ce4ab3201ec33e533f201604fe75ccb7fce871
Steven Riggs <steven.riggs@icloud.com>
2019-12-14 12:30:45 -0500

    add border

--- src/components/records/Records.js
+++ src/components/records/Records.js
@@ -5,11 +5,11 @@ import RecordsHeader from './RecordsHeader';
 
 function Records() {
   return (
-    <div className='debug d-flex flex-column w-100'>
+    <div className='d-flex flex-column w-100'>
         <div className='d-flex flex-grow-0'>
             <RecordsHeader />
         </div>
-        <div className='debug d-flex flex-grow-1'>
+        <div className='d-flex flex-grow-1'>
             RecordsTable
         </div>
         <div className='d-flex flex-grow-0'>
--- src/components/records/RecordsHeader.js
+++ src/components/records/RecordsHeader.js
@@ -3,7 +3,7 @@ import SearchControl from './SearchControl';
 
 function RecordsHeader() {
   return (
-    <div className='d-flex flex-row flex-grow-1 justify-content-between align-items-center p-2'>
+    <div className='d-flex flex-row flex-grow-1 justify-content-between align-items-center border p-2'>
         <div className='d-flex'>
             <h4>Records Title</h4>
         </div>
