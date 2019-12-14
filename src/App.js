4b933cd53f3caf4d1e03605df30d427eefbc6b82
Steven Riggs <steven.riggs@icloud.com>
2019-12-14 10:24:08 -0500

    add header global

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Container, Row} from 'react-bootstrap';
+import Header from './components/global/Header';
 
 function App() {
   return (
     <Container fluid className='vh-100'>
       <Row>
-        Header
+        <Header />
       </Row>
       <Row className='h-100'>
         Content
---
+++ src/components/global/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+
+function Header() {
+    return (
+        <Container fluid>
+            Header
+        </Container>
+    );
+}
+
+export default Header;
